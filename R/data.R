fcd468df0ef84821dfd86dfaba697b877a44a04c
Marijn <marijnjaarsma@factsconsultancy.nl>
2023-09-19 20:39:11 +0200

    Added data to package and documented it

---
+++ R/data.R
@@ -0,0 +1,9 @@
+#' Example graph from Dijkstra Wiki
+#' 
+#' \describe{
+#' \item{v1}{Starting vertex of edge}
+#' \item{v2}{Ending vertex of edge}
+#' \item{w}{Weight of the edge}
+#' }
+#' @source <https://en.wikipedia.org/wiki/Dijkstra%27s_algorithm>
+"wiki_graph"
